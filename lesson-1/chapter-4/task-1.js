5c0dba6ac25ca3a2d49e00c3a4eef49312d9bac4
lexusegorov <alexey.egorov.job@gmail.com>
2022-06-18 15:35:45 +0700

    Глава-4 задание 1

---
+++ lesson-1/chapter-4/task-1.js
@@ -0,0 +1,41 @@
+const user = {};
+user.name = 'John';
+user.surname = 'Smith';
+user.name = 'Pete';
+delete user.name;
+
+const isEmpty = (obj) => {
+  for(const key in obj){
+    return false
+  }
+  return true;
+}; 
+
+//Можно, потому что мы меняем свойства объекта, а не сам объект
+
+const salaries = {
+  John: 100,
+  Ann: 150,
+  Pete: 130,
+};
+
+let sum = 0;
+for (const key in salaries){
+  sum += salaries[key];
+}
+
+const menu = {
+  width: 200,
+  height: 300,
+  title: "My menu",
+}
+
+const multiplyNumeric = (obj) => {
+  for (const key in obj){
+    if(typeof( obj[key] ) === 'number'){
+      obj[key] *= 2;
+    }
+  }
+};
+
+multiplyNumeric(menu);
