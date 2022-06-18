8ffcb765e12047f2be0aaf42fa9180a909bc7d35
lexusegorov <alexey.egorov.job@gmail.com>
2022-06-18 13:18:53 +0700

    Задание 2

---
+++ lesson-1/task-2.js
@@ -0,0 +1,12 @@
+const someCode = (birthday) => alert(`Дата рождения: ${birthday}`);
+
+let name = 'Джон';
+let admin = name;
+
+alert(admin);
+
+let myPlanetName;
+let currentUserName;
+
+const BIRTHDAY = '18.04.1982';
+const age = someCode(BIRTHDAY);
